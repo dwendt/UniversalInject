06715f3548fe85b2ce1d4e9571b0385d283f43c3
Ditmar Wendt <dwendt@hackucf.org>
2014-05-27 20:02:24 -0400

    Cleaned up the IME a bit.

--- UInjectIME/main.cpp
+++ UInjectIME/main.cpp
@@ -1,56 +1,5 @@
-#include <windows.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include "imm.h"
-#include <tchar.h>
-#pragma comment(lib,"imm32.lib")
+#include "main.h"
 // Credit to the Chinese communities for much of the code in this file. 
-
-// Shared code segment. If this DLL is loaded into any process, 
-// this segment of code will point to the same physical memory in each of those processes. (...on the next episode of My Little DLL: Windows Is Magic)
-#pragma data_seg(".shared")
-DWORD CallBackData1=0;
-DWORD CallBackData2=0;
-DWORD CallBackData3=0;
-
-DWORD UnloadDllWhenExit=0;   // Unload the injected DLL on IME exit?
-DWORD LoadNextWhenActive=0;  // Go to the next IME if this IME is made active
-char pathToInjectableDLL[MAX_PATH]=""; // Full path to the injectable DLL
-#pragma data_seg()
-#pragma comment(linker,"/section:.shared,RWS")   
-
-#pragma region defines
-typedef DWORD (CALLBACK * POSTINJECTIONCALLBACK)(DWORD calldata1, DWORD calldata2,DWORD calldata3);
-
-// Handle to the injected DLL, and a pointer to the function to be called after injection(for giving the DLL extra data).
-HMODULE injectedDLL = NULL;
-POSTINJECTIONCALLBACK postInjectionCallback = NULL;
-
-BOOL ImeClass_Register(HINSTANCE hInstance);
-void ImeClass_Unregister(HINSTANCE hInstance);
-
-BOOL syncPostMessage(HIMC hIMC, UINT msg, WPARAM wParam, LPARAM lParam);
-
-// The IME window doesn't need to do anything.
-LRESULT WINAPI UIWndProc(HWND hUIWnd,UINT message,WPARAM wParam,LPARAM lParam) { return 0; }
-LRESULT WINAPI StatusWndProc(HWND hWnd,UINT message,WPARAM wParam,LPARAM lParam) { return 0; }
-LRESULT WINAPI CompWndProc(HWND hWnd,UINT message,WPARAM wParam,LPARAM lParam) { return 0; }
-LRESULT WINAPI CandWndProc(HWND hWnd,UINT message,WPARAM wParam,LPARAM lParam) { return 0; }
-
-// Necessary IME exports
-UINT WINAPI ImeEnumRegisterWord(REGISTERWORDENUMPROC lpfnRegisterWordEnumProc, LPCTSTR lpszReading, DWORD dwStyle, LPCTSTR lpszString, LPVOID lpData) { return 0; }
-UINT WINAPI ImeGetRegisterWordStyle(UINT nItem, LPSTYLEBUF lpStyleBuf) { return 0; }
-BOOL WINAPI ImeRegisterWord(LPCTSTR lpszReading, DWORD dwStyle, LPCTSTR lpszString) { return 0; }
-BOOL WINAPI ImeUnregisterWord(LPCTSTR lpszReading, DWORD dwStyle, LPCTSTR lpszString) { return 0; }
-BOOL WINAPI NotifyIME(HIMC hIMC,DWORD dwAction,DWORD dwIndex,DWORD dwValue) { return 0; }
-UINT WINAPI ImeToAsciiEx (UINT uVKey,UINT uScanCode,CONST LPBYTE lpbKeyState,LPDWORD lpdwTransKey,UINT fuState,HIMC hIMC) { return 0; }
-BOOL WINAPI ImeSetCompositionString(HIMC hIMC, DWORD dwIndex, LPCVOID lpComp, DWORD dwComp, LPCVOID lpRead, DWORD dwRead) { return 0; }
-BOOL WINAPI ImeProcessKey(HIMC hIMC,UINT uKey,LPARAM lKeyData,CONST LPBYTE lpbKeyState) { return 0; }
-LRESULT WINAPI ImeEscape(HIMC hIMC,UINT uSubFunc,LPVOID lpData) { return 0; }
-DWORD WINAPI ImeConversionList(HIMC hIMC,LPCTSTR lpSource,LPCANDIDATELIST lpCandList,DWORD dwBufLen,UINT uFlag) { return 0; }
-BOOL WINAPI ImeDestroy(UINT uForce) { return (uForce ? TRUE : 0); }
-BOOL WINAPI ImeSetActiveContext(HIMC hIMC,BOOL fFlag) { return TRUE; }
-#pragma endregion
 
 // Handles loading the real target DLL
 void injectDLL() {
